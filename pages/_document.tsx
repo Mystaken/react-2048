966a113ef64ea761a89567529aa8158462bf36d3
Mystaken <kgaotianx@gmail.com>
2019-07-29 16:40:20 -0400

    Fixed 'Prop className did not match error'

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
   public render() {
@@ -16,5 +17,30 @@ class MyDocument extends Document {
     );
   }
 }
+
+MyDocument.getInitialProps = async ctx => {
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      )
+    };
+  } finally {
+    sheet.seal();
+  }
+};
 
 export default MyDocument;
